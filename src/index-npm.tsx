246195b1642245e872570bbc026634d6746c530e
Steve <stevebarakat@gmail.com>
2021-09-09 14:26:53 -0400

    check in

--- src/index-npm.tsx
+++
@@ -1,8 +0,0 @@
-import { GlobalStyle } from "./shared/styles/GlobalStyle";
-
-export { GlobalStyle };
-
-export * from './components/RangeSlider/RangeSlider';
-export * from './components/VerticalRangeSlider/VerticalRangeSlider';
-export * from './components/DualRangeSlider/DualRangeSlider';
-export * from './components/DualVerticalRangeSlider/DualVerticalRangeSlider';
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import { GlobalStyle } from "./shared/styles/GlobalStyle";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <GlobalStyle />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export { GlobalStyle };
+
+export * from './components/RangeSlider/RangeSlider';
+export * from './components/VerticalRangeSlider/VerticalRangeSlider';
+export * from './components/DualRangeSlider/DualRangeSlider';
+export * from './components/DualVerticalRangeSlider/DualVerticalRangeSlider';
