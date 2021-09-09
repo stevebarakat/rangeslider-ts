fa79379d8143178b95253cc96c89131be5503a2b
Steve <stevebarakat@gmail.com>
2021-09-09 16:00:13 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-export {}
+import { GlobalStyle } from "./shared/styles/GlobalStyle";
+
+export { GlobalStyle };
+
+export * from "./components/RangeSlider/RangeSlider";
+export * from "./components/DualRangeSlider/DualRangeSlider";
+export * from "./components/VerticalRangeSlider/VerticalRangeSlider";
+export * from "./components/DualVerticalRangeSlider/DualVerticalRangeSlider";
